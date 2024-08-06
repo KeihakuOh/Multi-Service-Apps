24f3a8bd982635be66193d96af708e0431c181b9
KeihakuOh <w1123527817@akane.waseda.jp>
2024-08-06 23:52:50 +0900

    add other signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -9,4 +9,40 @@ it('returns a 201 on successful signup', async () => {
       password: 'password'
     })
     .expect(201);
-},10000);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'alskdflaskjfd',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'alskdflaskjfd',
+      password: 'p'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'alskjdf'
+    })
+    .expect(400);
+});
