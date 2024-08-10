67b81186936e9969ffdf3bfc79035413bb7b3fca
KeihakuOh <w1123527817@akane.waseda.jp>
2024-08-10 18:33:41 +0900

    fix current test bug

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,12 +1,22 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global { 
+      signin: () => Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  const mongo = await MongoMemoryServer.create();
+  mongo = await MongoMemoryServer.create(); // 変数を再定義しないように修正
   const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {});
@@ -26,3 +36,24 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async (): Promise<string[]> => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  if (!cookie) {
+    throw new Error('Cookie not set'); // cookie が undefined の場合、エラーを投げる
+  }
+
+  return cookie;
+};
